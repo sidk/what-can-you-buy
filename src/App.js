163a7ccda9ee0d3b6d0bc2fe26b1a2b4f14461a2
Sid Krishnan <krishnan.sid@gmail.com>
2020-07-11 12:43:27 -0400

    the tool

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [savings, setSavings] = React.useState(0);
+  const [lowerRange, setLowerRange] = React.useState(0);
+  const [higherRange, setHigherRange] = React.useState(0);
+
+  const calculateRange = (e) => {
+    setSavings(e.target.value);
+    setLowerRange(((25000 - e.target.value) * 100) / 5);
+    setHigherRange(((25000 - e.target.value) * 100) / 3);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <input
+        type="range"
+        value={savings}
+        onChange={calculateRange}
+        onInput={calculateRange}
+        min="0"
+        max="25000"
+      />
+      <p>
+        Savings on the amount of rent we currently pay:{" "}
+        <strong>${savings}</strong>
+      </p>
+      <div>
+        <h2>
+          ${(lowerRange / 1000).toFixed(2)}k {"<"} Your House Price {"<"} $
+          {(higherRange / 1000).toFixed(2)}k
+        </h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The lower range assumes an opportunity cost of 1%, and the higher one
+          assumes opportunity cost of 3%. Most likely the true opportunity cost
+          will be somewhere in between.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p> We also assume 2% maintenance cost of owning house </p>
+        <p>
+          {" "}
+          What is opportunity cost? It's the money we lose by investing in a
+          house as opposed to the stock market.
+        </p>
+      </div>
     </div>
   );
 }
